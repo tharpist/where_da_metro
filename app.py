7f7ee655f15dff57fb5d66f6c9d4ae3e66182687
Tyler Tharp <tharpta@gmail.com>
2021-07-06 18:15:56 -0400

    figured out how to url format

--- app.py
+++ app.py
@@ -8,17 +8,20 @@ app = Flask(__name__)
 @app.route('/', methods=['POST', 'GET'])
 def home():
     if request.method == 'POST':
-        station = get_station(request.form['nm'])
-        data = fetch_data(station)
-        message = send_message(data)
-        return redirect(url_for('station', a_data=message))
+        station_name = request.form['nm']
+        return  redirect(url_for('station',
+         station_name=station_name))
     if request.method == 'GET':
         print(request.method)
         return render_template('index.html')
 
-@app.route('/<a_data>')
-def station(a_data):
-    return f"<h1>{a_data}<h1>"
+@app.route('/<station_name>')
+def station(station_name):
+    station_url = get_station(station_name)
+    train_data = fetch_data(station_url)
+    message = send_message(train_data)
+    #return render_template('input.html')
+    return f"<h1>{message}<h1>"
 
 #default port:
 if __name__ == '__main__':
